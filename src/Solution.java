1570d328010384e0c07a1b9d73e4e7fd1368fa64
Srinivas <pranith13@gmail.com>
2017-02-11 15:34:09 -0600

    basics in string

--- src/Solution.java
+++ src/Solution.java
@@ -51,4 +51,19 @@ public class Solution {
         }
         return true;
     }
+
+    public String reverseString(String s) {
+
+        char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(word);
+    }
 }
