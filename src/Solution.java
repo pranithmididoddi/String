9f6922c68394488d942e84c7c135f620aca8f118
Srinivas <pranith13@gmail.com>
2017-02-12 13:04:48 -0600

    basics in string

--- src/Solution.java
+++ src/Solution.java
@@ -110,5 +110,39 @@ public class Solution {
         return sb.toString();
     }
 
+    public int myAtoi(String str) {
+
+        str=str.trim();
+        if(str.length()==0 || str==null) return 0;
+        char flag='+';
+
+        int i=0;
+        double result=0;
+
+        if(str.charAt(0)=='-'){
+            flag='-';
+            i++;
+        }else if(str.charAt(0)=='+'){
+            i++;
+        }
+
+        while(i<str.length() && str.charAt(i)>='0' && str.charAt(i)<='9'){
+            result=result*10+(str.charAt(i)-'0');
+            i++;
+        }
+
+        if(flag=='-')
+            result=-result;
+
+
+        if(result>Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
+
+        if(result<Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+
+        return (int) result;
+    }
+
 
 }
