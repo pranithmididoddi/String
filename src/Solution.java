194a941fab5ad2f24b92662ef568dd411fee2e42
Srinivas <pranith13@gmail.com>
2017-02-12 19:42:04 -0600

    basics in string

--- src/Solution.java
+++ src/Solution.java
@@ -144,5 +144,45 @@ public class Solution {
         return (int) result;
     }
 
+    public String validIPAddress(String IP) {
+        if(IP.length()==0) return "Neither";
+        if(IPV4Check(IP)) return "IPv4";
+
+        if(isIP6(IP)) return "IPv6";
+
+        return "Neither";
+    }
+
+    public boolean IPV4Check(String str){
+        if(str.charAt(0)=='.' || str.charAt(str.length()-1)=='.') return false;
+        String[] temp=str.split("\\.");
+        if(temp.length!=4) return false;
+
+        for(int i=0;i<temp.length;i++){
+            try{
+                if(temp[i].startsWith("0") && temp[i].length()>1) return false;
+                if(Integer.parseInt(temp[i])>255 || temp[i].charAt(0)=='-' || temp[i].charAt(0)=='+') return false;
+            }
+            catch(NumberFormatException e){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isIP6(String IP){
+        if(IP.charAt(0)==':' || IP.charAt(IP.length()-1)==':')return false;
+        String[] temp = IP.split(":");
+        if(temp.length!=8)return false;
+        for(int i=0;i<8;i++){
+            if(temp[i].length()>4||temp[i].length()==0)return false;
+            for(int j=0;j<temp[i].length();j++){
+                if((temp[i].charAt(j)>='0' && temp[i].charAt(j)<='9') || (temp[i].charAt(j)>='a' && temp[i].charAt(j)<='f') || (temp[i].charAt(j)>='A' && temp[i].charAt(j)<='F')){}
+                else return false;
+            }
+        }
+        return true;
+    }
+
 
 }
