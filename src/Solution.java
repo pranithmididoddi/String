34b2a6d0b63194161237f2c0e67c2f61dd5a19a6
Srinivas <pranith13@gmail.com>
2017-02-14 01:44:57 -0600

    vowel reverse

--- src/Solution.java
+++ src/Solution.java
@@ -255,4 +255,19 @@ public class Solution {
         return true;
     }
 
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] array=new int[26];
+
+        for(int i=0;i<magazine.length();i++){
+            array[magazine.charAt(i)-'a']++;
+        }
+
+        for(int j=0;j<ransomNote.length();j++){
+            if(--array[ransomNote.charAt(j)-'a']<0) return false;
+        }
+
+        return true;
+    }
+
+
 }
