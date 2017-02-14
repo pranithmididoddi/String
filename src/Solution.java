3f2ccf51de3e28dc9b465324cf66ea08e356e205
Srinivas <pranith13@gmail.com>
2017-02-13 23:36:02 -0600

    vowel reverse

--- src/Solution.java
+++ src/Solution.java
@@ -227,6 +227,32 @@ public class Solution {
         return new String(my);
 
     }
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result=new ArrayList<>();
+        int length=s.length();
 
+        for(int i=1;i<4 && i<length-2 ;i++){
+            for(int j=i+1;j<i+4 && j<length-1;j++){
+                for(int k=j+1;k<j+4 && k<length; k++){
+                    String s1=s.substring(0,i), s2=s.substring(i,j), s3=s.substring(j,k), s4=s.substring(k,length);
+
+                    if(isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)){
+                        result.add(s1+"."+s2+"."+s3+"."+s4);
+
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean isValid(String string){
+        int length=string.length();
+
+        if(length>3 || length==0 || (string.charAt(0)=='0' && length>1) || Integer.parseInt(string) > 255)
+            return false;
+
+        return true;
+    }
 
 }
