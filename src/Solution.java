e2a2adb9f140e8d0cb1b0b9d151dec48d6db5719
Srinivas <pranith13@gmail.com>
2017-03-09 14:35:49 -0600

    vowel reverse

--- src/Solution.java
+++ src/Solution.java
@@ -406,4 +406,30 @@ public class Solution {
 
         return sb.toString();
     }
+
+    public boolean isValid(String s) {
+
+        Map<Character, Character> map=new HashMap<>();
+        map.put('[',']');
+        map.put('{','}');
+        map.put('(',')');
+
+
+        Stack<Character> stack=new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char curr=s.charAt(i);
+
+            if(map.keySet().contains(curr)){
+                stack.push(curr);
+            }else if(map.values().contains(curr)){
+                if(!stack.empty() && map.get(stack.peek())==curr){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
 }
